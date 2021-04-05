1e86bbd7f90ee20129cd442d2cbcb3a5fea500b1
kibetcollinskenduiwa <kibetcollinskenduiwa@gmail.com>
2021-04-05 07:17:48 +0300

    Plagiarism

---
+++ plag.py
@@ -0,0 +1,39 @@
+# Word embedding is the process of converting textual data to array of numbers-sci-kit-learn library
+
+import os
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.metrics.pairwise import cosine_similarity
+
+# IfidVectorizer is used in conversting textual data to array of numbers
+# cosine similarity is used to compute similarity
+
+student_files = [doc for doc in os.listdir() if doc.endswith('.txt')]
+student_notes =[open(File).read() for File in  student_files]
+
+# Two lamda functions are used  for conversion and computation
+
+vectorize = lambda Text: TfidfVectorizer().fit_transform(Text).toarray()
+similarity = lambda doc1, doc2: cosine_similarity([doc1, doc2])
+
+vectors = vectorize(student_notes)
+s_vectors = list(zip(student_files, vectors))
+
+# Function to compute similarity
+
+def check_plagiarism():
+    plagiarism_results = set()
+    global s_vectors
+    for student_a, text_vector_a in s_vectors:
+        new_vectors =s_vectors.copy()
+        current_index = new_vectors.index((student_a, text_vector_a))
+        del new_vectors[current_index]
+        for student_b , text_vector_b in new_vectors:
+            sim_score = similarity(text_vector_a, text_vector_b)[0][1]
+            student_pair = sorted((student_a, student_b))
+            score = (student_pair[0], student_pair[1],sim_score)
+            plagiarism_results.add(score)
+    return plagiarism_results
+
+  for data in check_plagiarism():
+    print(data)
+    
